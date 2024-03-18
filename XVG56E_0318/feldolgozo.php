7710911eed23276e5f0e63547da34cbdd3a5c606
LorincAkos <lorinc.akos13@gmail.com>
2024-03-18 15:26:32 +0100

    ora

---
+++ XVG56E_0318/feldolgozo.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>HTML5 Űrlap_XVG56E</h2>
+
+    <?php
+
+    echo ("<strong>Név: </strong>" . $_POST['nev'] . "<br> <br>");
+
+    echo ("<strong>PIN kód: </strong>" . $_POST['kod'] . "<br> <br>");
+
+    echo ("<strong>Gyümölcs: </strong>" . $_POST['gyumolcs'] . "<br> <br>");
+
+    echo ("<strong>Életkor: </strong>" . $_POST['eletkor'] . "<br> <br>");
+
+    echo ("<strong>Lábméret: </strong>" . $_POST['labmeret'] . "<br> <br>");
+
+    echo ("<strong>Önbizalom: </strong>" . $_POST['onbizalom'] . "<br> <br>");
+    ?>
+
+    <a href="XVG56E_urlap.html">Vissza az űrlapra.</a>
+</body>
+</html>
